72fff5079ffe2335c2f31b4315dd39b3588c3e96
Sayakdas-1803 <sayakdas1803@gmail.com>
2025-06-17 11:19:47 +0530

    Time series analysis on air passenger data set.R

--- time series analysis on air passenger data set.R
+++ time series analysis on air passenger data set.R
@@ -7,24 +7,24 @@ library(ggplot2)
 library(forecast)
 
 #Basic time series plot..................
-ggplot(data, aes(x = as.Date(paste0(Month, "-01")), y =X.Passengers)) +  # Convert "YYYY-MM" to full date
+ggplot(data, aes(x = as.Date(paste0(Month, "-01")), y =X.Passengers)) +  
   geom_line(color = "steelblue", linewidth = 1) +
   labs(title = "Monthly Air Passengers (1949-1960)", x = "Year", y = "Passengers") +
   theme_minimal()
 
 #seasonal component...............
 decomp <- stl(ts_data, s.window = "periodic")
-autoplot(decomp) + theme_minimal()  # Uses forecast::autoplot
+autoplot(decomp) + theme_minimal()  
 
 #3..............
 #arima Models.................
 library(tseries)
-adf.test(ts_data)  # If p > 0.05, difference the data
-ts_diff <- diff(ts_data)  # First differencing
+adf.test(ts_data)  
+ts_diff <- diff(ts_data)  
 
 #Auto ARIMA model..................
 model <- auto.arima(ts_data)
-summary(model)  # Check ARIMA(p,d,q) coefficients
+summary(model)  
 
 #forecast and plot result............
 forecast_data <- forecast(model, h = 24)  # Forecast 2 years
